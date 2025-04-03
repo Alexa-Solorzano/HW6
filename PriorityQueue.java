dac2bc8b2f6e91a32530b6df7387ab9e476d216d
Alexa-Solorzano <150977874+Alexa-Solorzano@users.noreply.github.com>
2025-04-02 22:28:53 -0500

    Update PriorityQueue.java

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,7 +1,7 @@
 
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Alexa Solorzano COMP 272
  *
  *   Note, additional comments provided throughout this source code
  *   is for educational purposes
